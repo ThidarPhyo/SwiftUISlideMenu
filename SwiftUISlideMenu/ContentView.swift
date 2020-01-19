930e37fbe029e79987d6077d2beae645b55fe45e
ThidarPhyo <thidarphyo@comquas.tech>
2020-01-19 13:41:29 +0630

    Initial Commit

---
+++ SwiftUISlideMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISlideMenu
+//
+//  Created by Thidar Phyo on 1/19/20.
+//  Copyright © 2020 Thidar Phyo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
